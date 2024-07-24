003104e888d94b0e9055def2a01baff7f694f9f6
flyingculprit <suryan2021ai@gmail.com>
2024-07-24 10:41:44 +0530

    Process optimization

---
+++ celery.py
@@ -0,0 +1,12 @@
+from celery import Celery
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+    return celery
+
+celery = make_celery(app)
